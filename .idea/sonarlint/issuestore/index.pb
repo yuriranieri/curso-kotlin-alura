54accb8297ea155a3feaf05ac4042f7e28d10156
yuri.ranieri <yuri.ranieri@compasso.com.br>
2022-03-21 13:59:44 -0300

    modulo 3 curso lidando com exception

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-[
-+src/main/kotlin/execution/testaCondicoes.kt,4\4\44e394aa3c7e4844eb7753fdfeab2982dd0bc052
--- src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -1,34 +1,29 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.execution.testaContasDiferentes
-import br.com.alura.bytebank.model.Cliente
-import br.com.alura.bytebank.model.conta.Conta
-import br.com.alura.bytebank.model.conta.ContaCorrente
-import br.com.alura.bytebank.model.interfaces.Autenticavel
+import br.com.alura.bytebank.exception.SaldoInsuficienteException
 
 fun main() {
+    println("início main")
+    funcao1()
+    println("fim main")
+}
 
-    val fran = object : Autenticavel {
-        val nome: String = "Fran"
-        val cpf: String = "222.222.222-22"
-        override val senha: Int = 1000
-
-        override fun autentica(senha: Int): Boolean {
-            return this.senha == senha
-        }
+fun funcao1() {
+    println("início funcao1")
+    try {
+        funcao2()
+    } catch (e: SaldoInsuficienteException) {
+        e.printStackTrace()
+        println("SaldoInsuficienteException foi pegada")
     }
+    println("fim funcao1")
+}
 
-    println("autentica obj anonimo: ${fran.autentica(1000)}")
-
-    val alex = Cliente(
-        nome = "Alex",
-        cpf = "111.111.111-11",
-        senha = 123
-    )
-    val contaCorrente = ContaCorrente(
-        titular = alex,
-        numero = 2222
-    )
-    testaContasDiferentes()
-    println("Total de contas ${Conta.total}")
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5) {
+        println(i)
+        throw SaldoInsuficienteException()
+    }
+    println("fim funcao2")
 }
